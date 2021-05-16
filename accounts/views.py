221933dd60203edf6dbd16b6a3c494ec7171778c
imrptsngh <imrptsngh@gmail.com>
2021-05-17 01:59:27 +0530

    Created 3 simple functions for acc Views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('home')
+
+def product(request):
+    return HttpResponse('product')
+
+def customer(request):
+    return HttpResponse('customer')
