2f580b4ed4f435f43c01c2a278131e5e5c5db67c
imrptsngh <imrptsngh@gmail.com>
2021-05-19 23:35:15 +0530

    Created a simple customer models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    phone = models.IntegerField(null=True)
+    email = models.EmailField(max_length=254, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
