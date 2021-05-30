5aec0493abe273e528d241c0fa03ab99208ea5c2
imrptsngh <imrptsngh@gmail.com>
2021-05-30 14:01:58 +0530

    Created Models & added it to Admin

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,30 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length=100, null=True)
-    phone = models.IntegerField(null=True)
+    phone = models.CharField(max_length=10, null=True)
     email = models.EmailField(max_length=254, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=100, null=True)
+    description = models.TextField(max_length=250, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    status = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    # customer = 
+    # product = 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=100, null=True)
