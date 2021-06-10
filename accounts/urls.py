21cb1465d2e3b031418cac41beaca53ec52fb286
imrptsngh <imrptsngh@gmail.com>
2021-06-10 23:28:17 +0530

    Update urls file with comments

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    # Product
     path('products/', views.product, name='product'),
+    
+    # Customer
     path('customer/<int:pk>', views.customer, name='customer'),
+
+    # Order
     path('create_order/<int:pk>', views.createOrder, name='create_order'),
     path('update_order/<int:pk>', views.updateOrder, name='update_order'),
     path('delete_order/<int:pk>', views.deleteOrder, name='delete_order'),
