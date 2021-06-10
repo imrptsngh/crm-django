8e12202c13e137d8bd45c54f029cbb0a8951a210
imrptsngh <imrptsngh@gmail.com>
2021-06-10 20:42:15 +0530

    Added form rendering and validation in views

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 def home(request):
@@ -27,4 +28,13 @@ def customer(request, pk):
     return render(request, "accounts/customer.html", context)
 
 def createOrder(request):
-    return render(request, "accounts/order_form.html")
+    form = OrderForm()
+    context = {
+        'form': form
+    }
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    return render(request, "accounts/order_form.html", context)
